1479b27c6e093f8088c0c322d26c3ccd4bcb734e
AnhDuc1235 <anhducdanghoang@gmail.com>
2025-12-04 00:49:38 +0700

    final-module-js

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -2,27 +2,27 @@ function Sidebar() {
     return `
     <div class="js-sidebar flex-col bg-black h-[calc(100vh-64px)] top-[64px] left-0 fixed z-[50] border-r border-[#333] transition-all duration-300">
         <div class="flex flex-col items-baseline gap-[20px] pl-[12px] pt-[20px]">
-            <div id="sidebar-home" class="js-home w-[170px] flex flex-row items-center justify-start gap-[10px] border hover:bg-gray-700 pb-[10px] rounded-[10px] ml-[11px] cursor-pointer js-spa-home">
+            <div id="sidebar-home" class="js-home w-[170px] flex flex-row items-center justify-start gap-[10px] border hover:bg-gray-800 pb-[10px] rounded-[8px] cursor-pointer js-spa-home">
                 <i class="fa-solid fa-house text-white text-2xl w-[40px]"></i>
                 <a href="/" class="text-white text-[13px] pointer-events-none">Home</a>
             </div>
             
-            <div id="sidebar-explore" class="js-explore w-[170px] flex flex-row items-center justify-start gap-[10px] border hover:bg-gray-700 pb-[10px] rounded-[10px] ml-[11px] cursor-pointer">
+            <div id="sidebar-explore" class="js-explore w-[170px] flex flex-row items-center justify-start gap-[10px] border hover:bg-gray-800 pb-[10px] rounded-[8px] cursor-pointer">
                 <i class="fa-regular fa-compass text-white text-2xl w-[40px]"></i>
                 <a href="#" class="text-white text-[13px]">Explore</a>
             </div>
 
-            <div class="js-library w-[170px] flex flex-row items-center justify-start gap-[10px] border hover:bg-gray-700 pb-[10px] rounded-[10px] ml-[11px]">
+            <div class="js-library w-[170px] flex flex-row items-center justify-start gap-[10px] border hover:bg-gray-800 pb-[10px] rounded-[8px] cursor-pointer">
                 <i class="fa-regular fa-bookmark text-white text-2xl w-[40px]"></i>
                 <a href="#" class="text-white text-[13px]">Library</a>
             </div>
             
-            <div class="js-signin js-login-trigger w-[170px] flex flex-row items-center justify-start gap-[10px] border hover:bg-gray-700 pb-[10px] rounded-[10px] ml-[11px] cursor-pointer">
-                <i class="fa-solid fa-address-card text-white text-2xl w-[40px]"></i>
+            <div class="js-signin js-login-trigger w-[170px] flex flex-row items-center justify-start gap-[10px] border hover:bg-gray-800 pb-[10px] rounded-[8px] cursor-pointer">
+                <i class="fa-regular fa-user text-white text-2xl w-[40px]"></i>
                 <a href="#" class="text-white text-[13px] pointer-events-none">Sign in</a>
             </div>
             
-            <div class="js-signin-full w-[230px] pt-[10px] pb-[10px] px-2 flex-col justify-center">
+            <div class="js-signin-full w-[150px] pt-[10px] pb-[10px] flex-col justify-center">
                 <button class="js-login-trigger w-full bg-[#1d1d1d] text-white font-semibold py-2 rounded-full mb-3 text-sm hover:bg-[#333] transition cursor-pointer">Sign in</button>
                 <p class="text-gray-300 text-[13px] text-center">Sign in to create & share playlists, get personalized recommendations, and more.</p>
             </div>
@@ -49,8 +49,8 @@ document.addEventListener("DOMContentLoaded", () => {
                 if (signinFull) signinFull.classList.remove("hidden");
                 if (signinSmall) signinSmall.classList.add("hidden");   //lỗi hiển thị signinSmall: không thêm xóa được hidden 
                 items.forEach(i => {
-                    i.classList.remove("flex-col", "w-[60px]", "text-center", "justify-center", "items-center");
-                    i.classList.add("flex-row", "w-[170px]", "justify-start", "ml-[11px]");
+                    i.classList.remove("flex-col", "w-[60px]", "text-center", "items-center");
+                    i.classList.add("flex-row", "w-[170px]");
                     const textLink = i.querySelector("a");
                     if (textLink) textLink.classList.remove("hidden");
                 });
@@ -60,8 +60,8 @@ document.addEventListener("DOMContentLoaded", () => {
                 if (signinFull) signinFull.classList.add("hidden");
                 if (signinSmall) signinSmall.classList.remove("hidden");
                 items.forEach(i => {
-                    i.classList.remove("flex-row", "w-[170px]", "justify-start", "ml-[11px]");
-                    i.classList.add("flex-col", "w-[60px]", "text-center", "justify-center", "items-center");
+                    i.classList.remove("flex-row", "w-[170px]");
+                    i.classList.add("flex-col", "w-[60px]", "text-center", "items-center");
                     const textLink = i.querySelector("a");
                     if (textLink) textLink.classList.add("hidden");
                 });
